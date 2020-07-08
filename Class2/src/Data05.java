2b961b5545fa360918d8162fce9c0d1b403bb77f
hjcom <tkdddnrgks3@gmail.com>
2020-07-08 17:03:53 +0900

    Add Data05

---
+++ Class2/src/Data05.java
@@ -0,0 +1,15 @@
+// 암시적인 형변환과 명시적인 형변환
+public class Data05 {
+    public static void main(String[] args){
+        byte a=1;
+        short b=128;
+        int c=32768;
+
+        b=a;    // 암시적인 형변환
+        System.out.println(b);
+
+        b=(short)c;     // 명시적인 형변환
+
+        System.out.println(b);  // 오버플로우가 발생되어 엉뚱한 값 출력
+    }
+}
