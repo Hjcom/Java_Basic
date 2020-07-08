6b610fe6667da8b91841f8c2e67458a75ae53076
hjcom <tkdddnrgks3@gmail.com>
2020-07-08 17:08:23 +0900

    Add Data06.java

---
+++ Class2/src/Data06.java
@@ -0,0 +1,10 @@
+// 실수 형태의 자료형 저장하기
+public class Data06 {
+    public static void main(String[] args){
+        double a=23.7;
+        float b=23.7f;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
