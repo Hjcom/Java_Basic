83a4fd955d8c058cf6d2ab5e276ce89b8a7663af
hjcom <tkdddnrgks3@gmail.com>
2020-07-08 16:58:38 +0900

    Add Data03

---
+++ Class2/src/Data03.java
@@ -0,0 +1,12 @@
+// 정수 상수를 저장하는 정수형 변수
+public class Data03 {
+    public static void main(String[] args) {
+        int a;  // 변수 선언
+        a=1;    // 변수에 값을 저장
+        System.out.println(a);
+
+        a=2;    // 변수값 변경
+        System.out.println(a);
+
+    }
+}
