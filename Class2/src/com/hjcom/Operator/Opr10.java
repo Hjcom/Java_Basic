605c3717ad584164ace1e1e7ebd8bcd73e6000a1
hjcom <tkdddnrgks3@gmail.com>
2020-07-10 18:03:32 +0900

    Add Opr10.java

---
+++ Class2/src/com/hjcom/Operator/Opr10.java
@@ -0,0 +1,10 @@
+package com.hjcom.Operator;
+// 비트 단위 쉬프트 연산자
+public class Opr10 {
+    public static void main(String[] args) {
+        byte x = 15;
+
+        System.out.println("x << 2 : " + (x << 2));
+        System.out.println("x >> 2 : " + (x >> 2));
+    }
+}
