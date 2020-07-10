dbd47b7330c5d39f1b75df608d8b2311ff029b03
hjcom <tkdddnrgks3@gmail.com>
2020-07-10 16:34:56 +0900

    Add Opr01_02.java

---
+++ Class2/src/Opr01_02.java
@@ -0,0 +1,15 @@
+public class Opr01_02 {
+    public static void main(String[] args){
+        String a="Apple";
+        String b="Banana";
+        String c=a+b;
+
+        System.out.println(c);
+
+        String str = "결과값 : ";
+        int n = 10;
+
+        System.out.println(str+n);
+        System.out.println("결과값 : "+n);
+    }
+}
