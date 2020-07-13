1779a36ff2e65aa52afae3836679d671c48c05db
hjcom <tkdddnrgks3@gmail.com>
2020-07-13 18:11:14 +0900

    Add For02.java

---
+++ Class2/src/com/hjcom/iteration/For02.java
@@ -0,0 +1,12 @@
+package com.hjcom.iteration;
+// 제어변수 값 출력하기
+public class For02 {
+    public static void main(String[] args) {
+        int i;
+
+        for (i=1; i<=4; i++)
+            System.out.println(i);     // 제어변수 i값을 출력
+
+        System.out.println("--->> " + i);
+    }
+}
