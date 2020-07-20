d2ca8de024516ba1f9fee47be2ca4d69e408ac42
hjcom <tkdddnrgks3@gmail.com>
2020-07-20 20:01:05 +0900

    Add MethodEx01.java (start method)

---
+++ Class2/src/com/hjcom/method/MethodEx01.java
@@ -0,0 +1,12 @@
+package com.hjcom.method;
+// Hello World 출력하는 메서드 정의
+
+public class MethodEx01 {
+    static void hello_func(){
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        hello_func();
+    }
+}
